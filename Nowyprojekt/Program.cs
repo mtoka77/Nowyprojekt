3b665f92fd112153040cacd7de70e34dc7b80691
Marcin Tokarz <mtoka1977@gmail.com>
2024-01-26 16:36:45 +0100

    Zadanie 5

--- Nowyprojekt/Program.cs
+++ Nowyprojekt/Program.cs
@@ -1,24 +1,66 @@
-﻿//homework 1
+﻿//homework 2
 
-using System.ComponentModel.Design;
+using System.ComponentModel.DataAnnotations;
 
-var name = "Marcin";
-var sex = "Male";
-var age = 46;
+int number = 1977197711;
+string numberAsString = number.ToString();
 
-if (sex == "Female" && age < 30)
+List<char> letters = numberAsString.ToList();
+
+int[] counters = new int[10];
+
+for (int i=0; i< counters.Length; i++)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    counters[i] = 0;
 }
-else if (name == "Ewa" && age == 30)
+foreach (char letter in letters)
 {
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex == "Male" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
+    if (letter == '0')
 { 
-    Console.WriteLine("NIC");
+        counters[0]++;
 }
+else if (letter == '1')
+{
+    counters[1]++;
+}
+else if (letter == '2')
+{
+    counters[2]++;
+}
+else if (letter == '3')
+{
+    counters[3]++;
+}
+else if (letter == '4')
+{
+    counters[4]++;
+}
+else if (letter == '5')
+{
+    counters[5]++;
+}
+else if (letter == '6')
+{
+    counters[6]++;
+}
+else if (letter == '7')
+{
+    counters[7]++;
+}
+else if (letter == '8')
+{
+    counters[8]++;
+}
+else if (letter == '9')
+{
+    counters[9]++;
+}
+}
+Console.WriteLine("W liczbie:" + number);
+for (int i = 0; i < counters.Length; i++)
+{
+    Console.WriteLine("cyfra " + i + " wystepuje " + counters[i] + " razy ");
+}
+
+
+
